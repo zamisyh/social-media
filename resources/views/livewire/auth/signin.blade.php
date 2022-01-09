d74ed2f1c1d29421e33450ea4285bea64c4fdc07
zam-dev <zamsyh.work@gmail.com>
2022-01-09 15:19:35 +0700

    Setup authentication and design

--- resources/views/livewire/auth/signin.blade.php
+++ resources/views/livewire/auth/signin.blade.php
@@ -1,3 +1,37 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
-</div>
+<div class="min-h-screen hero bg-base-200">
+    <div class="flex-col justify-center hero-content">
+      <div class="text-left">
+            <h1 class="mb-1 text-4xl font-bold">
+                Social Media App
+            </h1>
+            <p class="mb-2">
+                Bosan sendiri terus ? yuk cari teman disini
+            </p>
+      </div>
+      <div class="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="Input your email" class="input input-bordered">
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="Input your password" class="input input-bordered">
+            <label class="label">
+              <a href="#" class="label-text-alt">Forgot password?</a>
+            </label>
+          </div>
+          <div class="mt-6 form-control">
+            <button class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+      <div class="mt-2 mb-3">
+        <a href="{{ route('auth.signup') }}" class="text-1xl">Don't have any account ?</a>
+    </div>
+    </div>
+  </div>
--- resources/views/livewire/auth/signup.blade.php
+++ resources/views/livewire/auth/signup.blade.php
@@ -1,3 +1,46 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-</div>
+<div class="min-h-screen hero bg-base-200">
+    <div class="flex-col justify-center hero-content">
+      <div class="text-left">
+            <h1 class="mb-2 text-4xl font-bold">
+                Social Media App
+            </h1>
+            <p class="mb-2">
+                Bosan sendiri terus ? yuk cari teman disini
+            </p>
+      </div>
+      <div class="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
+        <div class="card-body">
+            <div class="form-control">
+                <label class="label">
+                    <span class="label-text">Username</span>
+                </label>
+                <input type="text" placeholder="Input your username" class="input input-bordered">
+            </div>
+            <div class="form-control">
+                <label class="label">
+                <span class="label-text">Email</span>
+                </label>
+                <input type="text" placeholder="Input your email" class="input input-bordered">
+            </div>
+            <div class="form-control">
+                <label class="label">
+                <span class="label-text">Password</span>
+                </label>
+                <input type="text" placeholder="Input your password" class="input input-bordered">
+            </div>
+            <div class="form-control">
+                <label class="label">
+                <span class="label-text">Confirm Password</span>
+                </label>
+                <input type="text" placeholder="Input your confirm password" class="input input-bordered">
+            </div>
+            <div class="mt-6 form-control">
+                <button class="btn btn-primary">Next</button>
+            </div>
+        </div>
+      </div>
+      <div class="mt-2 mb-3">
+        <a href="{{ route('auth.signin') }}" class="text-1xl">Already have an account ?</a>
+    </div>
+    </div>
+  </div>
