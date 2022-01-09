d181beb31ab8dda4c052a89fbab6bbe0d0eabb07
zam-dev <zamsyh.work@gmail.com>
2022-01-09 16:33:28 +0700

    Create signup page

--- app/Http/Livewire/Auth/Signup.php
+++ app/Http/Livewire/Auth/Signup.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Livewire\Auth;
 
+use Illuminate\Validation\Rules\Password;
 use Livewire\Component;
 
 class Signup extends Component
 {
+
+    public $email, $username, $password, $confirm_password;
+    public $isShowPassword, $isShowConfirmPassword, $isNextForm;
+
     public function render()
     {
         return view('livewire.auth.signup')->extends('layouts.app')->section('content');
+    }
+
+    public function updatedConfirmPassword()
+    {
+        $this->validate([
+            'confirm_password' => 'same:password'
+        ]);
+    }
+
+
+    public function formValidation()
+    {
+       $this->validate([
+            'email' => 'required|email|unique:users,email',
+            'username' => 'required',
+            'password' => [
+                'required',
+                Password::min(8)
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+            ],
+            'confirm_password' => 'required'
+        ]);
+
     }
 }
--- resources/views/livewire/auth/signup.blade.php
+++ resources/views/livewire/auth/signup.blade.php
@@ -10,33 +10,93 @@
       </div>
       <div class="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
         <div class="card-body">
-            <div class="form-control">
-                <label class="label">
-                    <span class="label-text">Username</span>
-                </label>
-                <input type="text" placeholder="Input your username" class="input input-bordered">
-            </div>
-            <div class="form-control">
-                <label class="label">
-                <span class="label-text">Email</span>
-                </label>
-                <input type="text" placeholder="Input your email" class="input input-bordered">
-            </div>
-            <div class="form-control">
-                <label class="label">
-                <span class="label-text">Password</span>
-                </label>
-                <input type="text" placeholder="Input your password" class="input input-bordered">
-            </div>
-            <div class="form-control">
-                <label class="label">
-                <span class="label-text">Confirm Password</span>
-                </label>
-                <input type="text" placeholder="Input your confirm password" class="input input-bordered">
-            </div>
-            <div class="mt-6 form-control">
-                <button class="btn btn-primary">Next</button>
-            </div>
+            @if ($isNextForm)
+                Tes Form
+            @else
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Username</span>
+                    </label>
+                    <input type="text" wire:model.lazy='username' placeholder="Input your username" class="input input-bordered @error('username')
+                        input-error
+                    @enderror">
+                    @error('username')
+                        <span class="text-error">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Email</span>
+                    </label>
+                    <input type="text" wire:model.lazy='email' placeholder="Input your email" class="input input-bordered @error('email')
+                        input-error
+                    @enderror">
+                    @error('email')
+                        <span class="text-error">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Password</span>
+                    </label>
+                    <label class="input-group">
+                        <input type="{{ $isShowPassword ? 'text' : 'password' }}" wire:model.lazy='password' placeholder="Input your password" class="input input-bordered @error('password')
+                            input-error
+                        @enderror">
+                        @if ($isShowPassword)
+                            <span wire:click='$set("isShowPassword", false)'>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd" />
+                                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
+                                </svg>
+                            </span>
+                        @else
+                            <span wire:click='$set("isShowPassword", true)'>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                        @endif
+                    </label>
+                    @error('password')
+                        <span class="text-error">{{ $message }}</span>
+                    @enderror
+
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Confirm Password</span>
+                    </label>
+                    <label class="input-group">
+                        <input type="{{ $isShowConfirmPassword ? 'text' : 'password' }}" wire:model.lazy='confirm_password' placeholder="Input your confirm password" class="input input-bordered @error('confirm_password')
+                        input-error
+                        @enderror">
+                        @if ($isShowConfirmPassword)
+                            <span wire:click='$set("isShowConfirmPassword", false)'>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd" />
+                                    <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.065 7 9.542 7 .847 0 1.669-.105 2.454-.303z" />
+                                </svg>
+                            </span>
+                        @else
+                            <span wire:click='$set("isShowConfirmPassword", true)'>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                    <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                        @endif
+                    </label>
+
+                    @error('confirm_password')
+                        <span class="text-error">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mt-6 form-control">
+                    <button wire:click='$set("isNextForm", true)' class="btn btn-primary">Next</button>
+                </div>
+            @endif
         </div>
       </div>
       <div class="mt-2 mb-3">
