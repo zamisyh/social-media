327615cb3e36036290898da5bfa74b01a2a94c8a
zam-dev <zamsyh.work@gmail.com>
2022-01-10 13:26:30 +0700

    Add field in table posts likes

--- database/migrations/2022_01_09_050536_create_posts_likes_table.php
+++ database/migrations/2022_01_09_050536_create_posts_likes_table.php
@@ -15,7 +15,14 @@ class CreatePostsLikesTable extends Migration
     {
         Schema::create('posts_likes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('like_id');
+            $table->integer('total');
             $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('like_id')->references('id')->on('likes')->onDelete('cascade')->onUpdate('cascade');
+
         });
     }
 
