9c23e104aa59c9db3a4267bc98462cd5ac30c605
zam-dev <zamsyh.work@gmail.com>
2022-01-10 13:26:02 +0700

    Add field in table posts_comments

--- database/migrations/2022_01_09_050452_create_posts_comments_table.php
+++ database/migrations/2022_01_09_050452_create_posts_comments_table.php
@@ -15,7 +15,13 @@ class CreatePostsCommentsTable extends Migration
     {
         Schema::create('posts_comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('comment_id');
             $table->timestamps();
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade')->onUpdate('cascade');
+
         });
     }
 
