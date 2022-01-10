70bf994095b309c0f6f23ef62e04e63a67d06a62
zam-dev <zamsyh.work@gmail.com>
2022-01-10 13:26:41 +0700

    Add field in table follows

--- database/migrations/2022_01_09_050548_create_follows_table.php
+++ database/migrations/2022_01_09_050548_create_follows_table.php
@@ -15,7 +15,11 @@ class CreateFollowsTable extends Migration
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
         });
     }
 
