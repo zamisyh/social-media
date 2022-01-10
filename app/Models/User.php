fee3819aadb3a0e3dcf942116ce312374b4d1eab
zam-dev <zamsyh.work@gmail.com>
2022-01-10 13:25:05 +0700

    Update model user for relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profiles()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
+
