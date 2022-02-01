c417b07fef954a640d155bdee8cb08e14c30e333
zam-dev <zamsyh.work@gmail.com>
2022-02-01 20:36:12 +0700

    Fix bug in following user

--- app/Http/Livewire/Components/SuggestionFollowing.php
+++ app/Http/Livewire/Components/SuggestionFollowing.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Components;
 
 use App\Models\Follow;
+use App\Models\FollowUser;
 use Livewire\Component;
 use App\Models\Profile;
 use Illuminate\Support\Facades\Auth;
@@ -14,7 +15,7 @@ class SuggestionFollowing extends Component
     use LivewireAlert;
     protected $listeners = ['loadMore'];
 
-    public $data_following, $foll;
+    public $data_following, $foll, $data_follow_user;
     public $amount = 5;
 
 
@@ -45,5 +46,17 @@ class SuggestionFollowing extends Component
         $follow->users()->attach([
             'user_id' => $id
         ]);
+
+        $this->alert('success','Succesfully following user');
+    }
+
+    public function unfollow($id)
+    {
+        $follow = Follow::where('user_id', Auth::user()->id)->first();
+        $follow->users()->detach([
+            'user_id' => $id
+        ]);
+
+        $this->alert('success', 'Successfully unfollow user');
     }
 }
--- resources/views/livewire/components/suggestion-following.blade.php
+++ resources/views/livewire/components/suggestion-following.blade.php
@@ -18,9 +18,20 @@
                             </div>
                         </div>
                         <div class="mt-1 button_follow">
-                            <svg xmlns="http://www.w3.org/2000/svg" wire:click='follows({{ $item->id }})' role="button" class="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
-                            </svg>
+                            @php
+                                $data = \App\Models\FollowUser::where('user_id', $item->id)->first();
+                            @endphp
+                            @if (!is_null($data))
+                                <svg xmlns="http://www.w3.org/2000/svg" wire:click='unfollow({{ $item->id }})' class="w-6 h-6 text-red-400" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M11 6a3 3 0 11-6 0 3 3 0 016 0zM14 17a6 6 0 00-12 0h12zM13 8a1 1 0 100 2h4a1 1 0 100-2h-4z" />
+                                </svg>
+                            @else
+                                <svg xmlns="http://www.w3.org/2000/svg" wire:click='follows({{ $item->id }})' role="button" class="w-6 h-6 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
+                                </svg>
+
+                            @endif
+
                         </div>
                     </div>
                 @endif
