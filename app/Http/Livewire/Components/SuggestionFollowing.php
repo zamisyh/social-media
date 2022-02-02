2fe38f7ecbe2a795ada18dc12afb9e6bdf27b95b
zam-dev <zamsyh.work@gmail.com>
2022-02-02 11:30:33 +0700

    Create ajax feeds content following user

--- app/Http/Livewire/Components/SuggestionFollowing.php
+++ app/Http/Livewire/Components/SuggestionFollowing.php
@@ -49,6 +49,7 @@ class SuggestionFollowing extends Component
 
         $this->alert('success','Succesfully following user');
         $this->emit('getCountFollowing');
+        $this->emit('following');
     }
 
     public function unfollow($id)
@@ -60,5 +61,6 @@ class SuggestionFollowing extends Component
 
         $this->alert('success', 'Successfully unfollow user');
         $this->emit('getCountFollowing');
+        $this->emit('following');
     }
 }
