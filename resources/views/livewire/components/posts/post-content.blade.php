7cd4e02f92d8c3412aae93cdf1022469beb62e0e
zam-dev <zamsyh.work@gmail.com>
2022-02-07 00:22:07 +0700

    Numbering comment

--- resources/views/livewire/components/posts/post-content.blade.php
+++ resources/views/livewire/components/posts/post-content.blade.php
@@ -63,6 +63,7 @@
 
         </div>
         <div class="flex chat" role="button">
+
            @if ($openFormComment)
                 <svg xmlns="http://www.w3.org/2000/svg" wire:click='closeComment({{ $item->id }})' class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
@@ -72,8 +73,20 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                 </svg>
            @endif
+            @php
+                $countComment = \App\Models\Comment::with('comment')->get();
+                $count = 0;
 
-           <span class="ml-2">0</span>
+                foreach ($countComment as $key => $value) {
+                    foreach ($value->comment as $key2 => $d) {
+                        if ($item->id == $d->post_id) {
+                            $count = $d->count();
+                        }
+                    }
+                }
+            @endphp
+
+           <span class="ml-2">{{ $count }}</span>
         </div>
     </div>
 
