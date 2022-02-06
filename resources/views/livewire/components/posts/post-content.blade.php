c8570da0720fb0b1e3d13413058dd30f44745de3
zam-dev <zamsyh.work@gmail.com>
2022-02-07 00:41:50 +0700

    Fix bug name in comments

--- resources/views/livewire/components/posts/post-content.blade.php
+++ resources/views/livewire/components/posts/post-content.blade.php
@@ -65,8 +65,8 @@
         <div class="flex chat" role="button">
 
            @if ($openFormComment)
-                <svg xmlns="http://www.w3.org/2000/svg" wire:click='closeComment({{ $item->id }})' class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                <svg xmlns="http://www.w3.org/2000/svg"  wire:click='closeComment({{ $item->id }})' class="w-6 h-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd" />
                 </svg>
            @else
                 <svg xmlns="http://www.w3.org/2000/svg" wire:click='openComment({{ $item->id }})' class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -88,7 +88,7 @@
                     @foreach ($comment->comment as $c)
                         @if ($c->post_id === $item->id)
                             <div>
-                                <span class="text-sm text-gray-600">{{ $item->profiles->name }}</span><br>
+                                <span class="text-sm text-gray-600">{{ $comment->profiles->name }}</span><br>
                                 <span class="text-xs text-gray-400">{{ $comment->created_at->diffForHumans() }}</span><br>
                                 <div class="mt-3 mb-5">
                                     <span class="text-sm text-gray-500"> {{ $comment->text }}</span>
